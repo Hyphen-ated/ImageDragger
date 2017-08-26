1ef5fe6ddee0376b9d1875d5826da228c1736aff
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-08-25 18:16:29 -0700

    scratch code with jcanvas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var bah = $("canvas").drawPolygon({
+  layer: true,
+  draggable: true,
+  fillStyle: "#6c3",
+  x: 100, y: 100,
+  radius: 50, sides: 5
+});
+
+var yeah = $('canvas').drawImage({
+  layer: true,
+  source: 'img/google.png',
+  x: 150, y: 150,
+  draggable: true,
+  rotate: 80
+});
+
