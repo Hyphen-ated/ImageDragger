0951c885f50c615cc45c332250a4fc340a222d87
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-08-29 20:18:04 -0700

    adding images kinda works now

--- js/main.js
+++ js/main.js
@@ -1,19 +1,102 @@
 var stage;
 var objects = [];
+var layerCount = 0;
+var droppedFiles = false;
 
 function init() {
     stage = new createjs.Stage("canvas");
 
-    createImage("http://i.imgur.com/ryngzX3.png");
-    createImage("http://i.imgur.com/EeK0vFk.png");
-    createImage("img/google.png");
+//    createImageByUrl("http://i.imgur.com/ryngzX3.png");
+//    createImageByUrl("http://i.imgur.com/EeK0vFk.png");
+//    createImageByUrl("img/google.png");
     stage.update();    
     
     $( "#layerlist" ).sortable({placeholder: "ui-state-highlight"});
     $( "#layerlist" ).disableSelection();
+    
+    $("#fileinput").on("change", handleFileInput);
+  
+    $main= $("#main");
+    $main.on('dragover dragenter', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $main.addClass('is-dragover');
+    })
+    .on('dragleave dragend drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $main.removeClass('is-dragover');
+    })
+    .on('drop', function(e) { 
+        e.preventDefault();
+        e.stopPropagation();
+        if(e.originalEvent.dataTransfer.files && e.originalEvent.dataTransfer.files.length > 0) {    
+            processImageFiles(e.originalEvent.dataTransfer.files);
+        }
+    });
 }
 
-var createImage = function(url) {
+
+function processImageFiles(files) {
+    for (var i = 0, f; f = files[i]; i++) {  
+        processImageFile(f);
+    }
+}
+
+function processImageFile(f) {      
+    //ignore other kinds of files
+    if (f.type.match('image.*')) {            
+        var reader = new FileReader();           
+        reader.onload = (function(thefile) {
+            return function(e) {
+                console.log("got there");
+                var img = document.createElement('img');
+                img.src = e.target.result;  
+                putBitmapOnCanvas(img);
+                ++layerCount;
+                createLayer(layerCount);
+            }
+        })(f);
+        reader.readAsDataURL(f);
+    }   
+}
+
+function onFreezeChange(freezeid, checked) {
+    //freezeid looks like "freeze3". checked is bool
+}
+
+
+function createLayer(layernum) {    
+    var text = "Layer " + layernum;
+    var id = "layer" + layernum;
+    var $li = $('<li class="ui-state-default layer" id="'+id+'">'+
+                '<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>'+
+                 text + '</li>")');
+    var $label = $('<label for="freeze' + layernum + '" class="lockbutton">'+
+                    'Freeze</label>');
+    var $checkbox = $('<input type="checkbox" id="freeze'+ layernum +'">');
+    $checkbox.change(function() {
+        onFreezeChange(this.id, this.checked);
+    });
+    $label.append($checkbox);                                                     
+
+    $li.append($label);            
+    $("#layerlist").prepend($li);
+    $("#layerlist").sortable('refresh');
+    
+}
+
+function createLayerFromInput() {
+    if(droppedFiles) {
+        processImageFiles(droppedFiles);
+    }
+}
+
+function handleFileInput(e) {
+    droppedFiles = e.target.files;
+}
+
+function createImageByUrl(url) {
     var image = document.createElement("img");
     image.crossOrigin = "";
     image.src = url;
@@ -22,7 +105,7 @@ var createImage = function(url) {
 
 
 var curItem;
-enableDrag = function (item) {
+function enableDrag(item) {
     item.on("mousedown", function(evt) {
         var offset = {  x:item.x-evt.stageX,
                         y:item.y-evt.stageY};
@@ -52,12 +135,16 @@ document.onkeydown = function(e) {
 }
 
 function handleImageLoad(event) {
-    var image = event.target;
-    var bitmap = new createjs.Bitmap(image);
+    var img = event.target;
+    putBitmapOnCanvas(img);
+}
+
+function putBitmapOnCanvas(img) {
+    var bitmap = new createjs.Bitmap(img);
     enableDrag(bitmap);
     bitmap.rotation = 0;
-    bitmap.regX = image.width/2;
-    bitmap.regY = image.height/2;
+    bitmap.regX = img.width/2;
+    bitmap.regY = img.height/2;
     bitmap.x = 100;
     bitmap.y = 100;
 
